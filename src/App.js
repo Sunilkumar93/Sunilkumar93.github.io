da04d47e6092097e5090ec0a53f5884de67ffa2c
Sunilkumar93 <sunilkumarsihag098@gmail.com>
2022-11-27 16:06:38 +0530

    portfolio update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
-
+import "./App.css";
+import AboutMe from "./Routes/AboutMe";
+import Home from "./Routes/Home";
+import Navbar from "./components/Navbar";
+import AllRoutes from "./Routes/AllRoutes";
+import Skills from "./Routes/Skills";
+import Project from "./Routes/Project";
+import Contact from "./Routes/Contact";
 function App() {
   return (
     <div className="App">
-     sunil kumar
+      <Navbar />
+      <AllRoutes />
+      <Home />
+      <AboutMe />
+      <Skills />
+      <Project />
+      <Contact />
     </div>
   );
 }
